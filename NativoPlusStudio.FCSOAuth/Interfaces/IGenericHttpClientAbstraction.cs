1814c5f6ad6bfc6d94efb47c45b0e24285d46b2f
ninorodriguez <63418357+ninorodriguez@users.noreply.github.com>
2020-04-13 09:24:51 -0700

    Add FCSOAuth project
    - Removed the json file
    - Add the polly service

---
+++ NativoPlusStudio.FCSOAuth/Interfaces/IGenericHttpClientAbstraction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NativoPlusStudio.FCSOAuth
+{
+    public interface IGenericHttpClientAbstraction
+    {
+        Task<TResponse> DeleteAsync<TResponse>(string endpoint = "", string query = "") where TResponse : IBaseResponse, new();
+        Task<TResponse> GetAsync<TResponse>(string endpoint = "", string query = "") where TResponse : IBaseResponse, new();
+        Task<TResponse> Post<TRequest, TResponse>(TRequest request, string endpoint = "") where TResponse : IBaseResponse, new();
+        Task<TResponse> PostAsync<TResponse>(IList<KeyValuePair<string, string>> request, string endpoint = "") where TResponse : IBaseResponse, new();
+        Task<TResponse> PutAsync<TRequest, TResponse>(TRequest request, string endpoint = "") where TResponse : IBaseResponse, new();
+    }
+}
